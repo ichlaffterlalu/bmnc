9da9641286d26b3e0bbd1f8446b2bc22ab3b5716
Ichlasul Affan <ichlaffterlalu@gmail.com>
2018-06-01 13:15:50 +0700

    Fixed register errors

--- akun/views.py
+++ akun/views.py
@@ -45,75 +45,65 @@ def registrasi(request):
         return render(request, html, response)
 
     if request.method == 'POST':
-            role = request.POST.get('role', '')
-            username = request.POST.get('username', '')
-            password = request.POST.get('topik', '')
-            NoIdentitas = request.POST.get('NoIdentitas', '')
-            nama = request.POST.get('nama', '')
-            TempatLahir = request.POST.get('TempatLahir', '')
-            TanggalLahir = request.POST.get('TanggalLahir', '')
-            email = request.POST.get('email', '')
-            nohp = request.POST.get('nohp', '')
-            statusKemahasiswaan = request.POST.get('statusKemahasiswaan', '')
-            idUniversitas = request.POST.get('idUniversitas', '')
+        role = request.POST.get('role', '')
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        NoIdentitas = request.POST.get('NoIdentitas', '')
+        nama = request.POST.get('nama', '')
+        TempatLahir = request.POST.get('TempatLahir', '')
+        TanggalLahir = request.POST.get('TanggalLahir', '')
+        email = request.POST.get('email', '')
+        nohp = request.POST.get('nohp', '')
+        statusKemahasiswaan = request.POST.get('statusKemahasiswaan', '')
+        idUniversitas = request.POST.get('idUniversitas', '')
+        print(role)
+        print(username)
+        print(password)
+        print(NoIdentitas)
+        print(nama)
+        print(TempatLahir)
+        print(TanggalLahir)
+        print(email)
+        print(nohp)
+        print(statusKemahasiswaan)
+        print(idUniversitas)
 
-            if role == "Dosen" or role =="Staf":
-                c = connection.cursor()
-                try:
-                    c.execute('SELECT id FROM narasumber ORDER BY id desc limit 1;')
-                    lastId = c.fetchone()[0]
-                    c.execute('INSERT INTO Narasumber(id, nama, email, tempat, tanggal, no_hp, jumlah_berita, rerata_kata, id_universitas) \
-                        VALUES (%d, %s, %s, %s, %s, %s, %d, %d, %s);',
-                        [lastID+1, nama, email, TempatLahir, TanggalLahir, nohp, 0, 0, idUniversitas])
-
-                    c.execute('INSERT INTO PENGGUNA(username, password, id) \
-                        VALUES (%s, %s, %d);',
-                        [username,password, lastID+1])
-
-                    request.session['registrasi'] = 'Berhasil menambah user'
-                except:
-                    request.session['registrasi'] = 'Error ketika menambah user (username sudah ada)'
-                finally:
-                    c.close()
-
-
-            if role == "Mahasiswa":
-                c = connection.cursor()
-                try:
-                    c.execute('SELECT id FROM narasumber ORDER BY id desc limit 1;')
-                    lastId = c.fetchone()[0]
-                    c.execute('INSERT INTO Narasumber(id, nama, email, tempat, tanggal, no_hp, jumlah_berita, rerata_kata) \
-                        VALUES (%d, %s, %s, %s, %s, %s, %d, %d, %s);',
-                        [lastID+1, nama, email, TempatLahir, TanggalLahir, nohp, 0, 0])
-
-                    c.execute('INSERT INTO Mahasiswa(id_narasumber, npm,status) \
-                        VALUES (%d, %d, %s);',
-                        [lastID+1, NoIdentitas, statusKemahasiswaan])
-
-                    c.execute('INSERT INTO PENGGUNA(username, password, id) \
-                        VALUES (%s, %s, %d);',
-                        [username,password, lastID+1])
-
-                    request.session['registrasi'] = 'Berhasil menambah user'
-                except:
-                    request.session['registrasi'] = 'Error ketika menambah user (username sudah ada)'
-                finally:
-                    c.close()
-
-
-
-
+        if role == "Dosen" or role =="Staf":
             c = connection.cursor()
             try:
                 c.execute('SELECT id FROM narasumber ORDER BY id desc limit 1;')
                 lastId = c.fetchone()[0]
-                c.execute('INSERT INTO Narasumber(id, nama, email, tempat, tanggal, no_hp, jumlah_berita, rerata_kata) \
-                    VALUES (%d, %s, %s, %s, %s, %s, %d, %d);',
-                    [lastID+1, nama, email, TempatLahir, TanggalLahir, nohp, 0, 0])
+                c.execute('INSERT INTO Narasumber(id, nama, email, tempat, tanggal, no_hp, jumlah_berita, rerata_kata, id_universitas) \
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);',
+                    [lastId+1, nama, email, TempatLahir, TanggalLahir, nohp, 0, 0, idUniversitas])
 
-                c.execute('INSERT INTO PENGGUNA(username, password, id) \
-                    VALUES (%s, %s, %d);',
-                    [username,password, lastID+1])
+                c.execute('INSERT INTO PENGGUNA(username, password, id_narasumber) \
+                    VALUES (%s, %s, %s);',
+                    [username, password, lastId+1])
+
+                request.session['registrasi'] = 'Berhasil menambah user'
+            except:
+                request.session['registrasi'] = 'Error ketika menambah user (username sudah ada)'
+            finally:
+                c.close()
+
+        elif role == "Mahasiswa":
+            c = connection.cursor()
+            try:
+                c.execute('SELECT id FROM narasumber ORDER BY id desc limit 1;')
+                lastId = c.fetchone()[0]
+
+                c.execute('INSERT INTO Narasumber(id, nama, email, tempat, tanggal, no_hp, jumlah_berita, rerata_kata, id_universitas) \
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);',
+                    [lastId+1, nama, email, TempatLahir, TanggalLahir, nohp, 0, 0, idUniversitas])
+
+                c.execute('INSERT INTO Mahasiswa(id_narasumber, npm, status) \
+                    VALUES (%s, %s, %s);',
+                    [lastId+1, NoIdentitas, statusKemahasiswaan])
+
+                c.execute('INSERT INTO PENGGUNA(username, password, id_narasumber) \
+                    VALUES (%s, %s, %s);',
+                    [username, password, lastId+1])
 
                 request.session['registrasi'] = 'Berhasil menambah user'
             except:
